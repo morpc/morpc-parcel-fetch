24931d632a21cd33a0e9d3690ffcb5ac7a07a66a
Jordan <jinskeep@morpc.org>
2024-11-14 15:24:41 -0500

    working prototype

--- morrow-parcel-fetch.py
+++ morrow-parcel-fetch.py
@@ -53,15 +53,43 @@ jurisdictionsPartsRaw = morpc.load_spatial_data(JURISDICTIONS_PARTS_FEATURECLASS
 jurisdictionsPartsRaw = jurisdictionsPartsRaw.to_crs('epsg:3735')
 
 # %%
-addr_url = 'https://engineer.co.morrow.oh.us/arcgis/rest/services/Morrow_OtherLayers/MapServer/1'
+parcel_url = "https://engineer.co.morrow.oh.us/arcgis/rest/services/Morrow_ParcelRelated_new/MapServer/0"
 
 # %%
-r = requests.get('https://engineer.co.morrow.oh.us/arcgis/rest/services/Morrow_OtherLayers/MapServer/1/query?outFields=*&where=1%3D1&f=geojson&resultOffset=0&resultRecordCount=1000')
+parcels = morpcParcels.gdf_from_services(parcel_url)
 
 # %%
-r.json()
+morpcParcels.download_and_unzip_archive(url='https://share.pivotpoint.us/oh/morrow/cama/', filename='morrowoh.zip', temp_dir='./morrow_data/cama', keep_zip=True)
 
 # %%
-morpcParcels.gdf_from_services(addr_url)
+morrow_parcels = pyogrio.read_dataframe('./morrow_data/parcels/Morrow_Parcels.shp').set_index('Name')
+morrow_building = morpcParcels.extract_fields_from_cama(zip_path='./morrow_data/cama/morrowoh.zip', filename='GovernmaxBuildingExtract.txt', columns=['PropertyNumber', 'UseCode', 'YearBuilt']).set_index('PropertyNumber')
+morrow_dwelling = morpcParcels.extract_fields_from_cama(zip_path='./morrow_data/cama/morrowoh.zip', filename='GovernmaxDwellingExtract.txt', columns=['PropertyNumber', 'UseCode', 'YearBuilt']).set_index('PropertyNumber')
+morrow_dwelling = morrow_dwelling.join(morrow_parcels[['geometry', 'A_Acreage']]).dropna()
+morrow_building = morrow_building.join(morrow_parcels[['geometry', 'A_Acreage']]).dropna()
+morrow_parcels = pd.concat([morrow_dwelling, morrow_building])
+morrow_parcels = morrow_parcels.rename(columns = {'A_Acreage':'acres','UseCode':'land_use','YearBuilt':'year_built'})
+morrow_parcels = morrow_parcels[['acres', 'land_use', 'year_built', 'geometry']]
+morrow_parcels = gpd.GeoDataFrame(morrow_parcels, geometry='geometry').to_crs('3735')
+morrow_parcels['county'] = 'Morrow'
+
+# %%
+morrow_building = morpcParcels.extract_fields_from_cama(zip_path='./morrow_data/cama/morrowoh.zip', filename='GovernmaxBuildingExtract.txt')
+morrow_dwelling = morpcParcels.extract_fields_from_cama(zip_path='./morrow_data/cama/morrowoh.zip', filename='GovernmaxDwellingExtract.txt')
+
+# %%
+morrow_building.loc[morrow_building['UseCode'].str.startswith('40')==True][['PropertyNumber', 'UseCode', 'UnitCount']].drop_duplicates()
+
+# %%
+itables.show(morrow_building.loc[morrow_building['PropertyNumber']=='A01-001-00-235-07'])
+
+# %%
+morrow_building['UnitCount'] = [pd.to_numeric(x) for x in morrow_building['UnitCount']]
+
+# %%
+morrow_building
+
+# %%
+morrow_dwelling.groupby('PropertyNumber').agg({'CardNumber':'count'}).sort_values('CardNumber', ascending=False)
 
 # %%
