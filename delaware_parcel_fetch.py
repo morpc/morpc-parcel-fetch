a1292e415e88d6ad78a2a5481a4a8a05d095f804
Jordan <jinskeep@morpc.org>
2024-11-19 08:42:58 -0500

    add archive of files

--- delaware_parcel_fetch.py
+++ delaware_parcel_fetch.py
@@ -40,32 +40,32 @@ import morpcParcels
 STANDARD_GEO_VINTAGE = 2023
 JURISDICTIONS_PARTS_FEATURECLASS_FILEPATH = "../morpc-censustiger-standardize/output_data/morpc-standardgeos-census-{}.gpkg".format(STANDARD_GEO_VINTAGE)
 JURISDICTIONS_PARTS_FEATURECLASS_LAYER = "JURIS-COUNTY"
-print("Data: {0}, layer={1}".format(JURISDICTIONS_PARTS_FEATURECLASS_FILEPATH, JURISDICTIONS_PARTS_FEATURECLASS_LAYER))
-
-# %%
 INPUT_DIR = "./input_data"
+print("Data: {0}, layer={1}".format(JURISDICTIONS_PARTS_FEATURECLASS_FILEPATH, JURISDICTIONS_PARTS_FEATURECLASS_LAYER))
 
 inputDir = os.path.normpath(INPUT_DIR)
 if not os.path.exists(inputDir):
     os.makedirs(inputDir)
 
 jurisdictionsPartsRaw = morpc.load_spatial_data(JURISDICTIONS_PARTS_FEATURECLASS_FILEPATH, layerName=JURISDICTIONS_PARTS_FEATURECLASS_LAYER, archiveDir=inputDir)
-jurisdictionsPartsRaw = jurisdictionsPartsRaw.to_crs('epsg:3735')
 
 # %%
-auditor_addr_url = "https://services2.arcgis.com//ziXVKVy3BiopMCCU//arcgis//rest//services//Address_Point//FeatureServer//0"
+addr_url = "https://services2.arcgis.com//ziXVKVy3BiopMCCU//arcgis//rest//services//Address_Point//FeatureServer//0"
 
 # %%
-auditor_addr = morpcParcels.gdf_from_services(url = auditor_addr_url)
+auditor_addr = morpcParcels.gdf_from_services(url = addr_url)
+if not os.path.exists('./input_data/delaware_data/addr/'):
+    os.makedirs('./input_data/delaware_data/addr/')
+auditor_addr.to_file("./input_data/delaware_data/addr/delaware_addr.gpkg", driver='GPKG')
 
 # %%
-url = "https://services2.arcgis.com//ziXVKVy3BiopMCCU//arcgis//rest//services//Parcel//FeatureServer//0"
+parcels_url = "https://services2.arcgis.com//ziXVKVy3BiopMCCU//arcgis//rest//services//Parcel//FeatureServer//0"
 
 # %%
-delaware_parcels = morpcParcels.gdf_from_services(url = url)
-if not os.path.exists('./delaware_data/'):
-    os.makedirs('./delaware_data/')
-delaware_parcels.to_file("./delaware_data/delaware_parcels.gpkg", driver='GPKG')
+delaware_parcels = morpcParcels.gdf_from_services(url = parcels_url)
+if not os.path.exists('./input_data/delaware_data/parcels/'):
+    os.makedirs('./input_data/delaware_data/parcels/')
+delaware_parcels.to_file("./input_data/delaware_data/parcels/delaware_parcels.gpkg", driver='GPKG')
 
 # %%
 parcels = pyogrio.read_dataframe("C:\\Users\\jinskeep\OneDrive - Mid-Ohio Regional Planning Commission\\Desktop\\parcels\\parcels.shp")
@@ -74,16 +74,19 @@ parcels = pyogrio.read_dataframe("C:\\Users\\jinskeep\OneDrive - Mid-Ohio Region
 units = pd.DataFrame(auditor_addr[['PARCEL_NO', 'LSN']].groupby('PARCEL_NO').size()).rename(columns = {0:'UNITS'})
 
 # %%
-parcels = parcels[['PARCEL_NO', 'CLASS', 'YRBUILT', 'CALC_ACRE', 'geometry']].set_index('PARCEL_NO').join(units)
+parcels = parcels[['PARCEL_NO', 'CLASS', 'YRBUILT', 'ACRES', 'geometry']].set_index('PARCEL_NO').join(units)
 
 # %%
 parcels = parcels.loc[(~parcels['UNITS'].isna()) & (parcels['YRBUILT']>0)].drop_duplicates()
 
 # %%
-parcels.loc[(parcels['CALC_ACRE'] > .75 )& (parcels['CLASS'].str.startswith('51')), 'housing_unit_type'] = "SF-LL"
-parcels.loc[(parcels['CALC_ACRE'] <= .75) & (parcels['CLASS'].str.startswith('51')), 'housing_unit_type'] = "SF-SL"
-parcels.loc[parcels['CLASS'].str.startswith(('52', '53', '54', '55')), 'housing_unit_type'] = "SF-A"
-parcels.loc[parcels['CLASS'].str.startswith('4'), 'housing_unit_type'] = "MF"
+parcels.loc[(parcels['ACRES'] > .75 )& (parcels['CLASS'].str.startswith(('51', '501', '502', '503', '504', '505'))), 'TYPE'] = "SF-LL"
+parcels.loc[(parcels['ACRES'] <= .75) & (parcels['CLASS'].str.startswith(('51', '501', '502', '503', '504', '505'))), 'TYPE'] = "SF-SL"
+parcels.loc[parcels['CLASS'].str.startswith(('52', '53', '54', '55')), 'TYPE'] = "SF-A"
+parcels.loc[parcels['CLASS'].str.startswith('4'), 'TYPE'] = "MF"
+
+# %%
+parcels = parcels.loc[parcels['TYPE']!='nan']
 
 # %%
 parcels = gpd.GeoDataFrame(parcels, geometry='geometry').to_crs('epsg:3735')
@@ -102,18 +105,12 @@ parcels['YRBUILT'] = [pd.to_numeric(x) for x in parcels['YRBUILT']]
 parcels = parcels.sjoin(jurisdictionsPartsRaw[['PLACECOMBO', 'geometry']])
 
 # %%
-pd.DataFrame(parcels.loc[parcels['YRBUILT']>2020].groupby(['PLACECOMBO', 'YRBUILT']).agg({'UNITS':'sum'})).rename(columns={0:'UNITS'}).reset_index().pivot(index='PLACECOMBO', columns='YRBUILT',values= 'UNITS')
-
-# %%
-pd.DataFrame(parcels.loc[parcels['YRBUILT']>2020].groupby(['YRBUILT', 'housing_unit_type']).size()).rename(columns={0:'UNITS'})
-
-# %%
-parcels_filt = parcels.loc[parcels['YRBUILT']>2020]
+parcels = parcels.drop(columns=['index_right'])
 
 # %%
 (plotnine.ggplot()
     + plotnine.geom_map(jurisdictionsPartsRaw.loc[jurisdictionsPartsRaw['COUNTY']=='Delaware'], fill="None", color='black')
-    + plotnine.geom_jitter(parcels_filt, plotnine.aes(x='x', y='y', size = 'UNITS', fill = 'housing_unit_type'), color="None")
+    + plotnine.geom_jitter(parcels.loc[parcels['YRBUILT']>2020], plotnine.aes(x='x', y='y', size = 'UNITS', fill = 'TYPE'), color="None")
     + plotnine.theme(
         panel_background=plotnine.element_blank(),
         axis_text=plotnine.element_blank(),
@@ -124,5 +121,11 @@ parcels_filt = parcels.loc[parcels['YRBUILT']>2020]
    + plotnine.scale_size_radius(range=(.2,5), breaks = (1,50, 100, 250, 400))
  + plotnine.guides(size=plotnine.guide_legend(override_aes={'color':'black'}))
 )
+
+# %%
+pd.DataFrame(parcels.loc[parcels['YRBUILT']>2020].groupby(['YRBUILT', 'TYPE']).size()).rename(columns={0:'UNITS'})
+
+# %%
+pd.DataFrame(parcels.loc[parcels['YRBUILT']>2020].groupby(['PLACECOMBO', 'YRBUILT']).agg({'UNITS':'sum'})).rename(columns={0:'UNITS'}).reset_index().pivot(index='PLACECOMBO', columns='YRBUILT',values= 'UNITS')
 
 # %%
